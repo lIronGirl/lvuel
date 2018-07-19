975c05ad3ebebd569d6bdbe8a9d327d3cf9cea2e
Jessie <1518583297@qq.com>
2018-07-20 07:29:40 +0800

    add icons

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -2,8 +2,8 @@
     <div class="icons">
         <swiper :options="swiperOption">
     <!-- slides -->
-            <swiper-slide>
-                <div class="icon" v-for="icon of iconList" :key="icon.id">
+            <swiper-slide v-for="(page, index) of pages" :key="index">
+                <div class="icon" v-for="icon of page" :key="icon.id">
                     <img class="icon-img" :src="icon.imgUrl">
                     <p class="icon-desc">{{ icon.desc }}</p>
                 </div>
@@ -15,8 +15,11 @@
 <script>
 export default {
   name: 'HomeIcons',
-  data() {
+  data () {
     return {
+      swiperOption: {
+        autoplay: false
+      },
       iconList: [{
         id: '0001',
         imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/01/1516938549_25686.png',
@@ -27,33 +30,44 @@ export default {
         desc: '自驾游'
       }, {
         id: '0003',
-        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/01/1529646252_29632.png',
+        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/01/1516938549_25686.png',
         desc: '玩水季'
       }, {
         id: '0004',
-        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/01/1514474245_92479.png',
+        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2017/12/1514474349_75047.png',
         desc: '驴悦亲子游'
       }, {
         id: '0005',
-        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/01/1525256515_44403.png',
+        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/01/1516938549_25686.png',
         desc: '国际机票'
       }, {
         id: '0006',
-        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/01/1530699074_45895.png',
+        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2017/12/1514474349_75047.png',
         desc: '特价门票'
       }, {
         id: '0007',
-        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/01/1523252783_60535.png',
+        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/01/1516938549_25686.png',
         desc: '签到'
       }, {
         id: '0008',
-        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/01/1514474202_85264.png',
+        imgUrl: 'https://pics.lvjs.com.cn/pics/super/2017/12/1514474349_75047.png',
         desc: '签证'
       }, {
         id: '0009',
         imgUrl: 'https://pics.lvjs.com.cn/pics/super/2018/03/1520416990_47578.png',
         desc: '长隆'
-      }, ]
+      } ]
+    }
+  },
+  computed: {
+    pages () {
+      const PERPAGE = 8
+      let pages = []
+      let pageCount = Math.ceil(this.iconList.length / PERPAGE)
+      for (let i = 0; i < pageCount; i++) {
+        pages.push(this.iconList.slice(i * PERPAGE, i * PERPAGE + PERPAGE))
+      }
+      return pages
     }
   }
 }
@@ -61,6 +75,7 @@ export default {
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
   .icons >>> .swiper-container
     height 0
     padding-bottom 50%
@@ -75,4 +90,5 @@ export default {
     .icon-desc
       margin-top 8%
       color $darkTextColor
+      ellipsis()
 </style>
