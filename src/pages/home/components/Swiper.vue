78bbe665fb0a7d4d7332815280ab6415872da7c3
Jessie <1518583297@qq.com>
2018-07-22 22:40:30 +0800

    格式调整

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -12,41 +12,41 @@
 </template>
 
 <script>
-export default {
-  name: 'HomeSwiper',
-  data () {
-    return {
-      swiperOption: {
-        autoplay: {
-          delay: 2000,
-          disableOnInteraction: false
+  export default {
+    name: 'HomeSwiper',
+    data () {
+      return {
+        swiperOption: {
+          autoplay: {
+            delay: 2000,
+            disableOnInteraction: false
+          },
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          loop: true
         },
-        pagination: {
-          el: '.swiper-pagination'
-        },
-        loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgurl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
-      }, {
-        id: '0002',
-        imgurl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180713_CPM_11053.jpg'
-      }]
+        swiperList: [{
+          id: '0001',
+          imgurl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+        }, {
+          id: '0002',
+          imgurl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180713_CPM_11053.jpg'
+        }]
+      }
     }
   }
-}
 </script>
 
 <style lang="stylus" scoped>
-.wrapper >>> .swiper-pagination-bullet-active
-    background #fff
+  .wrapper >>> .swiper-pagination-bullet-active
+      background #fff
 
-.wrapper
-  overflow hidden
-  width 100%
-  height 0
-  padding-bottom 24.53%
-  .swiper-img
+  .wrapper
+    overflow hidden
     width 100%
+    height 0
+    padding-bottom 24.53%
+    .swiper-img
+      width 100%
 </style>
