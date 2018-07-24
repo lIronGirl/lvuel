19f5f491efdbe48911bc11ae7fd6c035819a6bd9
Jessie <1518583297@qq.com>
2018-07-24 23:46:48 +0800

    新建 推荐组件

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -12,30 +12,30 @@
 </template>
 
 <script>
-  export default {
-    name: 'HomeSwiper',
-    data () {
-      return {
-        swiperOption: {
-          autoplay: {
-            delay: 2000,
-            disableOnInteraction: false
-          },
-          pagination: {
-            el: '.swiper-pagination'
-          },
-          loop: true
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false
         },
-        swiperList: [{
-          id: '0001',
-          imgurl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
-        }, {
-          id: '0002',
-          imgurl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180713_CPM_11053.jpg'
-        }]
-      }
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgurl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+      }, {
+        id: '0002',
+        imgurl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180713_CPM_11053.jpg'
+      }]
     }
   }
+}
 </script>
 
 <style lang="stylus" scoped>
