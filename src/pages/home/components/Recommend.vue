2d37d7e0e18a4ded10390eadbf6b85f408982f21
Jessie <1518583297@qq.com>
2018-07-26 22:16:41 +0800

    添加推荐列表

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,6 +1,14 @@
 <template>
 <div class="recommend">
   <div class="recommend-title">热销推荐</div>
+  <ul class="recommend-list">
+    <li class="recommend-item">
+      <img src="http://img1.qunarzz.com/sight/p0/1803/12/126471e0966aca5ba3.img.jpg_200x200_f1286016.jpg" alt="北京国际鲜花港">
+      <p class="recommend-item-title">北京国际鲜花港</p>
+      <span class="recommend-item-rate">五星</span><span class="recommend-item-comment-count">1927条评论</span>
+      <span class="recommend-item-price"><span>￥19.9</span>起</span><span class="recommend-item-local">顺义区</span>
+    </li>
+  </ul>
 </div>
 </template>
 
