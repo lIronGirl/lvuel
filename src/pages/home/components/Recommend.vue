963b2b2cdb7d6f5c80f14efa4bf59640a4be9efd
Jessie <1518583297@qq.com>
2018-07-30 23:19:23 +0800

    修改推荐列表样式

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -5,8 +5,13 @@
     <li class="recommend-item">
       <img src="http://img1.qunarzz.com/sight/p0/1803/12/126471e0966aca5ba3.img.jpg_200x200_f1286016.jpg" alt="北京国际鲜花港">
       <p class="recommend-item-title">北京国际鲜花港</p>
-      <span class="recommend-item-rate">五星</span><span class="recommend-item-comment-count">1927条评论</span>
-      <span class="recommend-item-price"><span>￥19.9</span>起</span><span class="recommend-item-local">顺义区</span>
+      <div>
+        <span class="recommend-item-rate iconfont">&#58886;</span>
+        <span class="recommend-item-comment-count">1927条评论</span>
+      </div>
+      <div>
+        <span class="recommend-item-price"><span>￥19.9</span>起</span><span class="recommend-item-local">顺义区</span>
+      </div>
     </li>
   </ul>
 </div>
@@ -23,4 +28,19 @@ export default {
   background-color #eee
   line-height .8rem
   text-indent .2rem
+.recommend-list
+  margin-left .24rem
+  .recommend-item
+    padding .2rem 0
+    img
+      float left
+      margin-right .26rem
+      width 2rem
+      height 2rem
+    .recommend-item-title
+      margin-top .26rem
+      font-size .32rem
+    div
+      .iconfont
+        font-size .6rem
 </style>
